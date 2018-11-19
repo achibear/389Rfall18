123c063d061db84aa65d860f337afa4d2004df0d
Mingda Guo <ccees123@gmail.com>
2018-11-18 20:36:30 -0500

    completed 10

--- week/10/stub.py
+++ week/10/stub.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python2
 # from the git repo
 import md5py
+import socket
 
+host = "142.93.118.186"
+port = 1234
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+data = s.recv(1024)
+print(data)
+s.send('1'+'\n')
+data = s.recv(1024)
+print(data)
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
 
-message = ''    # original message here
-legit = ''      # a legit hash of secret + message goes here, obtained from signing a message
+message = 'fall18'    # original message here
+s.send(message+'\n')
+data = s.recv(1024)
+print(data)
+legit = data[len(data)-33:len(data)]      # a legit hash of secret + message goes here, obtained from signing a message
+legit = legit.strip()
 
+print(legit)
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
 fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
 
-malicious = ''  # put your malicious message here
+malicious = 'fall19'  # put your malicious message here
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
@@ -35,7 +50,9 @@ print(fake_hash)
 # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
 # craft padding to align the block as MD5 would do it
 # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+m1 = '\x80'+ '\x00'*39
+m2 = '\x80\x00\x00\x00\x00\x00\x00\x00'
+padding = m1 + m2
 
 # payload is the message that corresponds to the hash in `fake_hash`
 # server will calculate md5(secret + payload)
@@ -45,3 +62,16 @@ payload = message + padding + malicious
 
 # send `fake_hash` and `payload` to server (manually or with sockets)
 # REMEMBER: every time you sign new data, you will regenerate a new secret!
+s.send('2'+'\n')
+data = s.recv(1024)
+print(data)
+s.send(fake_hash+'\n')
+data = s.recv(1024)
+print(data)
+s.send(payload+'\n')
+data = s.recv(1024)
+print(data)
+data = s.recv(1024)
+print(data)
+data = s.recv(1024)
+print(data)
--- week/10/writeup/README.md
+++ week/10/writeup/README.md
@@ -1,18 +1,27 @@
 Writeup 10 - Crypto II
 =====
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Mingda Guo
+Section: 0101
 
 I pledge on my honor that I have not given or received anyunauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: Mingda Guo
 
 ## Assignment 10 Writeup
 
 ### Part 1 (70 Pts)
+For the first part, I first connec to the server using python socket. Then I saw there are options listed. So I sent '1' to the server. Follow its response, I sent my message "fall18" and got its hash.
 
+On the next step, I sent '2' fisrt and saw that the server was asking hash and data. Then I was trying to calculate the correct padding. I started the secret from 6 bytes. Since my message is 6 bytes and the end is 6 bytes, I got 64-12-8 = 44. So I created '\x80' and 43 '\x00' following secret + message. And changed the length byte to '\x60'.
+
+However, after sending those data, the server said that the hash is not correct. So I tried to increase secret length until it reached 10. And the flag I got is CMSC389R-{i_still_put_the_M_between_the_DV}.
 
 ### Part 2 (30 Pts)
 
+1. generating keys: gpg --gen-key then input the name and email address
+
+2. import public key: gpg --import public.asc
+
+3. encrypting message: gpg --output message.private --encrypt --recipient email@email.com message
 
